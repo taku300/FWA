f225a06901951f8c4dc05fb96e1ac70938eb4b9b
t-ookuma <siel0913@gmail.com>
2023-01-17 11:27:49 +0900

    header-title修正

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -1,4 +1,5 @@
 <x-layout.other-layout>
-    <x-slot name="en">test</x-slot>
-    <x-slot name="ja">テスト</x-slot>
+    <!-- header-title -->
+    <x-slot name="en">RECORDS</x-slot>
+    <x-slot name="ja">大会記録</x-slot>
 </x-layout.other-layout>
