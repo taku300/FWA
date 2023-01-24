cd9cc90b1e2103d45dfb80abf858af68c4e8dc1b
t-ookuma <siel0913@gmail.com>
2023-01-24 23:21:39 +0900

    修正

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -1,5 +1,8 @@
 <x-layout.other-layout>
     <!-- header-title -->
-    <x-slot name="en">RECORDS</x-slot>
+    <x-slot name="en">RECORD</x-slot>
     <x-slot name="ja">大会記録</x-slot>
+    
+    <!-- main -->
+    <session></session>
 </x-layout.other-layout>
