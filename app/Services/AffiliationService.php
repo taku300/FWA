85a65038f9f0c9ebdbb3c6fb4ea914cf0c19e94c
t-ookuma <siel0913@gmail.com>
2023-01-27 23:30:34 +0900

    Serviceクラス雛形作成

---
+++ app/Services/AffiliationService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class AffiliationService
+{
+    public function sample()
+    {
+        // 
+    }
+}
---
+++ app/Services/TopService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class TopService
+{
+    public function sample()
+    {
+        // 
+    }
+}
