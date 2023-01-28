8306eba5142ea3ac0aabf42211e34640e8d8190f
t-ookuma <siel0913@gmail.com>
2023-01-28 15:18:53 +0900

    ループ処理、値のセット、ケース分け追加

--- resources/views/components/list/lists.blade.php
+++ resources/views/components/list/lists.blade.php
@@ -1,50 +1,29 @@
+<?php
+
+/**
+ * $value->category = お知らせフラグ
+ * 1 = お知らせ
+ * 2 = 大会情報
+ */
+
+?>
+
+@foreach($newsList as $value)
 <div class="py-5 border-t-[1px] border-black">
     <a href="">
         <div class="flex items-center">
-            <p class="font-black mr-4">2022/12/08</p>
+            <p class="font-black mr-4">{{ $value->noticed_at }}</p>
+            @switch($value->category)
+            @case(1)
             <x-parts.button bg_color="orange" font_color="white">お知らせ</x-parts.button>
+            @break
+            @case(2)
+            <x-parts.button bg_color="orange" font_color="white">大会情報</x-parts.button>
+            @break
+            @endswitch
         </div>
-        <p class="mt-2 text-lg">ここに大会内容を入力してください</p>
-        <p class="opacity-60">※これは注意書きです</p>
+        <p class="mt-2 text-lg">{{ $value->detail }}</p>
+        <p class="opacity-60">{{ $value->note }}</p>
     </a>
 </div>
-<div class="py-5 border-t-[1px] border-black">
-    <a href="">
-        <div class="flex items-center">
-            <p class="font-black mr-4">2022/12/08</p>
-            <x-parts.button bg_color="red" font_color="white">大会情報</x-parts.button>
-        </div>
-        <p class="mt-2 text-lg">ここに大会内容を入力してください</p>
-        <p class="opacity-60">※これは注意書きです</p>
-    </a>
-</div>
-<div class="py-5 border-t-[1px] border-black">
-    <a href="">
-        <div class="flex items-center">
-            <p class="font-black mr-4">2022/12/08</p>
-            <x-parts.button bg_color="orange" font_color="white">お知らせ</x-parts.button>
-        </div>
-        <p class="mt-2 text-lg">ここに大会内容を入力してください</p>
-        <p class="opacity-60">※これは注意書きです</p>
-    </a>
-</div>
-<div class="py-5 border-t-[1px] border-black">
-    <a href="">
-        <div class="flex items-center">
-            <p class="font-black mr-4">2022/12/08</p>
-            <x-parts.button bg_color="orange" font_color="white">お知らせ</x-parts.button>
-        </div>
-        <p class="mt-2 text-lg">ここに大会内容を入力してください</p>
-        <p class="opacity-60">※これは注意書きです</p>
-    </a>
-</div>
-<div class="py-5 border-t-[1px] border-b-[1px] border-black">
-    <a href="">
-        <div class="flex items-center">
-            <p class="font-black mr-4">2022/12/08</p>
-            <x-parts.button bg_color="orange" font_color="white">お知らせ</x-parts.button>
-        </div>
-        <p class="mt-2 text-lg">ここに大会内容を入力してください</p>
-        <p class="opacity-60">※これは注意書きです</p>
-    </a>
-</div>
+@endforeach
