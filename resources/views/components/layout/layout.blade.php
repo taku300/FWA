aee037d1dc088c7e57e26ac7096f9a21804df65a
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-01-31 19:48:25 +0900

    要項・結果画面作成

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -10,13 +10,15 @@
     <!-- hero -->
     <x-layout.hero textColor="white" fontSize="black"></x-layout.hero>
     <!-- sidemenu -->
-    <x-layout.side-menu></x-layout.side-menu>
-      <!-- main -->
-      <main class="min-h-[2000px] bg-white ">
-          {{ $slot }}
-      </main>
-      <!-- footer -->
-      <x-layout.footer></x-layout.footer>
+    <x-layout.side-menu>
+        <x-layout.nav></x-layout.nav>
+    </x-layout.side-menu>
+        <!-- main -->
+        <main class="min-h-[2000px] bg-white ">
+            {{ $slot }}
+        </main>
+        <!-- footer -->
+        <x-layout.footer float="{{ isset($float) ? $float : 'none' }}"></x-layout.footer>
 </body>
 
 </html>
---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,24 @@
+<div class="mt-[17px]">
+    <a href="{{ route('index') }}">      <!-- トップに遷移 -->
+        <img class="h-[26px] m-auto" src="/images/layout/logo_white_strate.png" alt="福岡県ウエイトリフティング協会">
+    </a>
+</div>
+<nav class="text-white mt-[70px] ml-[40px] flex flex-col">
+    <a class="mb-3" href="{{ route('index') }}">TOP画面</a>     <!-- トップに遷移 -->
+    <p class="mb-3 mt-3">協会について</p>
+    <a class="mb-3" href="{{ route('about.index') }}">　協会概要</a>
+    <a class="mb-3" href="{{ route('history.index') }}">　協会の歩み</a>
+    <a class="mb-3" href="{{ route('plans.index') }}">　年間計画</a>
+    <p class="mb-3 mt-3">試合関連</p>
+    <a class="mb-3" href="{{ route('results.index') }}">　要項・結果</a>
+    <a class="mb-3" href="{{ route('records.index') }}">　大会記録</a>
+    <a class="mb-3 mt-3" href="{{ route('lifters.index') }}">選手紹介</a>
+    <a class="mb-3 mt-3" href="{{ route('news.index') }}">お知らせ</a>
+    <a class="mb-3 mt-3" href="{{ route('contact.index') }}">お問い合わせ</a>
+    <a class="mb-3 mt-3" href="{{ route('contact.index') }}">管理者</a>
+    <a class="mb-3" href="{{ route('admins.news.create') }}">　お知らせ登録</a>
+    <a class="mb-3" href="{{ route('admins.results.create') }}">　大会情報登録</a>
+    <a class="mb-3" href="{{ route('admins.lifters.create') }}">　選手登録</a>
+    <a class="mb-3" href="{{ route('admins.documents.edit') }}">　ドキュメント編集</a>
+    <a class="mb-3" href="{{ route('admins.top.edit') }}">　トップ編集</a>
+</nav>
