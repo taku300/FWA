0c1eaecb4abfad9ecc458e7f4b9d6a41e6d0d201
t-ookuma <siel0913@gmail.com>
2023-02-04 17:53:58 +0900

    input追加用jquery雛形作成

--- resources/js/jQuery.js
+++ resources/js/jQuery.js
@@ -1,10 +1,13 @@
 $(function() {
     // ボタン制御
     if($('#del-flg').val() == null){
-        $('#del-flg').attr('id', 'del-flg');
         $('#addButton').hidden();
     }else if ($('#del-flg').val() != null){
-        $('#del-flg').removeAttr('id');
         $('#addButton').show();
     }
+    // input追加
+    $('#addButton').on('click', function() {
+        $('#del-flg').removeAttr('id');
+        $('div div').append('<input id="del-flg">');
+    })
 });
