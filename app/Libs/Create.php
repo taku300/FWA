b54fc0252cd627e66009ad2390c225b7d31f3a61
t-ookuma <siel0913@gmail.com>
2023-02-05 00:57:28 +0900

    CRUD処理 create追加 動作未確認

---
+++ app/Libs/Create.php
@@ -0,0 +1,32 @@
+<?
+
+namespace App\Libs;
+
+use Illuminate\Http\Request;
+
+/**
+ * 新規登録処理
+ * 
+ * @param  App\Models\**  $model
+ * @param  array  $columns
+ * @param  Illuminate\Http\Request  $request
+ */
+class Create
+{
+    public static function create($model, $columns, $request)
+    {
+        foreach ($columns as $column) {
+            if (str_pos($column, '_path' !== false) && !empty($request->$column)) {
+                $path = $request->$column->store('public');
+                $model->$column = basename($path);
+            }
+            if (str_pos($column, '_path' !== false) && empty($request->$column)) {
+                $model->$column = $model->$column;
+            }
+            if (str_pos($column, '_path' === false)) {
+                $model->$column = $request->$column;
+            }
+        }
+        $model->save();
+    }
+}
