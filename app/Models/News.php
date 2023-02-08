898cd5f8a448418daa546eba30ad630f1b0000e3
t-ookuma <siel0913@gmail.com>
2023-02-08 19:43:37 +0900

    コード整理

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Route;
 use App\Models\NewsLink;
 use App\Models\NewsDocument;
 use App\Models\Result;
