612be1680f89cda0b06644ae56e946128823738d
t-ookuma <siel0913@gmail.com>
2023-02-20 00:10:50 +0900

    編集処理作成

--- app/Services/DocumentService.php
+++ app/Services/DocumentService.php
@@ -4,8 +4,29 @@ namespace App\Services;
 
 class DocumentService
 {
-    public function sample()
+    public function getTopImages()
     {
-        // 
+        $documents = [
+            1 => \CommonConst::ASSOCIATION_DOCUMENT_PATH . \CommonConst::DOCUMENT_LIST[1],
+            2 => \CommonConst::ASSOCIATION_DOCUMENT_PATH . \CommonConst::DOCUMENT_LIST[2],
+            3 => \CommonConst::ASSOCIATION_DOCUMENT_PATH . \CommonConst::DOCUMENT_LIST[3],
+        ];
+        return $documents;
+    }
+
+    public function updateDocument($request)
+    {
+        if (isset($request->top_image_path_1)) {
+            $request->file(\CommonConst::DOCUMENT_PATH_1)
+                ->storeAs(\CommonConst::ASSOCIATION_DOCUMENT_PATH, \CommonConst::DOCUMENT_LIST[1]);
+        }
+        if (isset($request->top_image_path_2)) {
+            $request->file(\CommonConst::DOCUMENT_PATH_2)
+                ->storeAs(\CommonConst::ASSOCIATION_DOCUMENT_PATH, \CommonConst::DOCUMENT_LIST[2]);
+        }
+        if (isset($request->top_image_path_3)) {
+            $request->file(\CommonConst::DOCUMENT_PATH_3)
+                ->storeAs(\CommonConst::ASSOCIATION_DOCUMENT_PATH, \CommonConst::DOCUMENT_LIST[3]);
+        }
     }
 }
