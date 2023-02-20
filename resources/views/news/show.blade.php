dd7e6b802fdd9ec1de4c9f8a990f2a436bf944dd
t-ookuma <siel0913@gmail.com>
2023-02-20 16:55:03 +0900

    ダウンロードパス修正

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -3,47 +3,47 @@
     <section>
         <!-- title/movie -->
         <x-layout.container>
-        <div class="relative">
-            <!-- 速報有りのとき速報画像を表示 -->
-            <img class="absolute -left-[5%] sp:-left-[4%] sp:top-3 z-50 h-[250px] sp:h-[146px]" src="{{ asset('images/parts/breaking_news.png') }}" alt="">
-            <div class="pt-[115px] sp:pt-[76px] pb-5 border-b border-black">
-                <p class="font-bold mb-2 pc:text-sm sp:text-xs">{{ $newsDetail['noticed_at'] }}</p>
-                <p class="text-xl pc:text-lg sp:text-sm">{{ $newsDetail['title'] }}</p>
-                <p class="opacity-60 mt-1 pc:text-sm sp:text-xs">{{ $newsDetail['noticed_at'] ? '※ ' . $newsDetail['noticed_at'] : '' }}</p>
+            <div class="relative">
+                <!-- 速報有りのとき速報画像を表示 -->
+                <img class="absolute -left-[5%] sp:-left-[4%] sp:top-3 z-50 h-[250px] sp:h-[146px]" src="{{ asset('images/parts/breaking_news.png') }}" alt="">
+                <div class="pt-[115px] sp:pt-[76px] pb-5 border-b border-black">
+                    <p class="font-bold mb-2 pc:text-sm sp:text-xs">{{ $newsDetail['noticed_at'] }}</p>
+                    <p class="text-xl pc:text-lg sp:text-sm">{{ $newsDetail['title'] }}</p>
+                    <p class="opacity-60 mt-1 pc:text-sm sp:text-xs">{{ $newsDetail['noticed_at'] ? '※ ' . $newsDetail['noticed_at'] : '' }}</p>
+                </div>
+                <img class="absolute top-0 -right-[5%] sp:-right-[2%] w-[164px] sp:w-[86px] h-[185px] sp:h-[97px] object-contain" src="{{ asset('images/parts/hashibiroko.png') }}" alt="">
             </div>
-            <img class="absolute top-0 -right-[5%] sp:-right-[2%] w-[164px] sp:w-[86px] h-[185px] sp:h-[97px] object-contain" src="{{ asset('images/parts/hashibiroko.png') }}" alt="">
-        </div>
-        @if ($newsDetail['iframe_path'])
-        <div class="my-9 sp:my-5 text-center">
-            <iframe class="inline-block w-[500px] sp:w-[90%] aspect-[700/400]" src="{{ $newsDetail['iframe_path'] }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-        @endif
-        <!-- 詳細 -->
-        <div class="sp:pt-5">
-            @if ($newsDetail['detail'])
-            <div class="mb-7">
-                <h2 class="mb-2 text-lg font-black">詳細</h2>
-                <p class="text-sm">{{ $newsDetail['detail'] }}</p>
+            @if ($newsDetail['iframe_path'])
+            <div class="my-9 sp:my-5 text-center">
+                <iframe class="inline-block w-[500px] sp:w-[90%] aspect-[700/400]" src="{{ $newsDetail['iframe_path'] }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             @endif
-            @if ($newsDetail['news_documents'])
-            <div class="mb-7">
-                <h2 class="mb-2 text-lg font-black">資料</h2>
-                @foreach ($newsDetail['news_documents'] as $document)
-                <a class="mb-5 text-sm block underline" href="{{ \Storage::get('news-documents/' . $document['document_path']) }}" download="file.pdf">{{ $document['title'] }}</a>
-                @endforeach
-            @endif
-            </div>
-            <div class="mb-7">
-            @if ($newsDetail['news_documents'])
-                <h2 class="mb-2 text-lg font-black">リンク</h2>
-                @foreach ($newsDetail['news_links'] as $link)
-                <a class="mb-5 text-sm block underline" href="{{ $link['link_path'] }}">{{ $link['title'] }}</a>
-                @endforeach
-            @endif
-            </div>
+            <!-- 詳細 -->
+            <div class="sp:pt-5">
+                @if ($newsDetail['detail'])
+                <div class="mb-7">
+                    <h2 class="mb-2 text-lg font-black">詳細</h2>
+                    <p class="text-sm">{{ $newsDetail['detail'] }}</p>
+                </div>
+                @endif
+                @if ($newsDetail['news_documents'])
+                <div class="mb-7">
+                    <h2 class="mb-2 text-lg font-black">資料</h2>
+                    @foreach ($newsDetail['news_documents'] as $document)
+                    <a class="mb-5 text-sm block underline" href="{{ \Storage::url('news-documents/' . $document['document_path']) }}" download="file.pdf">{{ $document['title'] }}</a>
+                    @endforeach
+                    @endif
+                </div>
+                <div class="mb-7">
+                    @if ($newsDetail['news_documents'])
+                    <h2 class="mb-2 text-lg font-black">リンク</h2>
+                    @foreach ($newsDetail['news_links'] as $link)
+                    <a class="mb-5 text-sm block underline" href="{{ $link['link_path'] }}">{{ $link['title'] }}</a>
+                    @endforeach
+                    @endif
+                </div>
 
-        </div>
+            </div>
         </x-layout.container>
     </section>
 </x-layout.layout>
