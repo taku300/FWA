2a2a709bfb1f76e31eeb6fa392cb0dd6364b4370
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-02-21 11:21:06 +0900

    Google タグ追加

--- resources/views/components/layout/head.blade.php
+++ resources/views/components/layout/head.blade.php
@@ -27,4 +27,14 @@
     <script src="{{ asset('js/news.js') }}" defer></script>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RDSPP0DDHM"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-RDSPP0DDHM');
+    </script>
 </head>
