1d596bbbe6d04b33165a861858178776492b4453
t-ookuma <siel0913@gmail.com>
2023-02-26 12:33:49 +0900

    インデント調整

--- resources/views/components/list/list.blade.php
+++ resources/views/components/list/list.blade.php
@@ -1,9 +1,8 @@
-
 <div class="flex items-center justify-between">
-        <div class="flex">
-            <p class="mr-4 pc:text-sm sp:text-xs">{{ $value['noticed_at'] }}</p>
-            <x-parts.button bgColor="{{ \CategoryConst::NEWS_CATEGORY_LIST[$value['category']]['color'] }}" fontColor="white">{{ \CategoryConst::NEWS_CATEGORY_LIST[$value['category']]['category'] }}</x-parts.button>
-        </div>
+    <div class="flex">
+        <p class="mr-4 pc:text-sm sp:text-xs">{{ $value['noticed_at'] }}</p>
+        <x-parts.button bgColor="{{ \CategoryConst::NEWS_CATEGORY_LIST[$value['category']]['color'] }}" fontColor="white">{{ \CategoryConst::NEWS_CATEGORY_LIST[$value['category']]['category'] }}</x-parts.button>
+    </div>
 </div>
 <p class="text-xl pc:text-lg sp:text-sm mt-2">{{ $value['title'] }}</p>
 <p class="opacity-60 mt-1 pc:text-sm sp:text-xs">{{ $value['note'] ? '※ ' . $value['note'] : '' }}</p>
