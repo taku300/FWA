5b7b1bdbbebc86659f9f9c65b6b537f236b69901
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-03-05 17:45:13 +0900

    ロゴの変更

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -8,15 +8,18 @@
                 <polygon points="0 0, 0 150, 350 0" class="fill-[#110781]"/>
             </svg>
             <svg width="400" height="190" class="opacity-60 fill-blue-800 hidden sp:block">
-                <polygon points="0 0, 0 130, 300 0" class="fill-[#110781]"/>
+                <polygon points="0 0, 0 140, 300 0" class="fill-[#110781]"/>
             </svg>
         </div>
-        <div class="fixed top-[25px] left-[25px] sp:top-[16px] sp:left-[16px] z-40">
+        <div class="fixed top-[25px] left-[25px] sp:top-[16px] sp:left-[16px] z-40 leading-4">
             <a href="{{ route('index') }}">     <!-- トップに遷移 -->
-                <img class="w-[230px] pc:w-[160px] sp:w-[160px]" src="{{ asset('images/layout/logo_white.png') . config('cache.update_date') }}" alt="福岡県ウエイトリフティング協会">
+                <p class="js-header-parts-text text-[10px] text-white font-black text-shadow">福岡県ウエイトリフティング協会</p>
+                <p class="js-header-parts-text text-[16px] text-white font-black text-shadow pc:text-[14px]">Fukuoka Weightlifting</p>
+                <p class="js-header-parts-text text-[16px] text-white font-black text-shadow pc:text-[14px]">Association</p>
+                <img class="js-header-parts-img mt-3 w-[50px] pc:w-[30px]" src="{{ asset('images/layout/logo_w_s.png') . config('cache.update_date') }}" alt="福岡県ウエイトリフティング協会">
             </a>
         </div>
-        <div class="fixed top-[25px] right-[25px] z-40 sp:top-[16px] sp:right-[16px] hover:bg-blue-200 active:bg-blue-300  focus:ring-blue-200">
+        <div class="hamburger fixed top-[35px] right-[25px] z-40 sp:top-[16px] sp:right-[16px] hover:bg-blue-200 active:bg-blue-300  focus:ring-blue-200">
             <button id="show-side-menu" class="js-header-parts w-[30px] h-[30px] relative opacity-60">
                 <span class="block w-full h-[2px] bg-blue-800 bg-opacity-80 absolute top-0"></span>
                 <span class="block w-full h-[2px] bg-blue-800 bg-opacity-80"></span>
