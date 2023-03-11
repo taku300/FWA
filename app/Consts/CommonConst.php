cf6f7521bc79c274ae1ef05811d7cc8b936476f7
t-ookuma <siel0913@gmail.com>
2023-03-11 13:02:31 +0900

    コンフリクト回避のため、こちらでコード整理

--- app/Consts/CommonConst.php
+++ app/Consts/CommonConst.php
@@ -27,6 +27,18 @@ class CommonConst
     const TOP_FILE_PATH_NAME_4 = 'top_image_path_4.png'; // top画像4
 
     /**
+     * top画像リスト
+     *
+     * @param  array
+     */
+    const TOP_IMAGE_LIST = [
+        self::TOP_FILE_PATH_1 =>  self::TOP_FILE_PATH_NAME_1,
+        self::TOP_FILE_PATH_2 =>  self::TOP_FILE_PATH_NAME_2,
+        self::TOP_FILE_PATH_3 =>  self::TOP_FILE_PATH_NAME_3,
+        self::TOP_FILE_PATH_4 =>  self::TOP_FILE_PATH_NAME_4,
+    ];
+
+    /**
      * ドキュメント
      */
     const DOCUMENT_PATH_1 = 'document_path_1';
@@ -37,22 +49,23 @@ class CommonConst
     const DOCUMENT_PATH_NAME_3 = 'document_path_3.pdf'; // 年間計画
 
     /**
+     * ドキュメントリスト
+     *
+     * @param  array
+     */
+    const DOCUMENT_LIST = [
+        self::DOCUMENT_PATH_1 =>  self::DOCUMENT_PATH_NAME_1,
+        self::DOCUMENT_PATH_2 =>  self::DOCUMENT_PATH_NAME_2,
+        self::DOCUMENT_PATH_3 =>  self::DOCUMENT_PATH_NAME_3,
+    ];
+
+    /**
      * 性別
      */
     const MEN = 1;
     const MEN_NAME = '男性';
     const WOMEN = 2;
     const WOMEN_NAME = '女性';
-
-    /**
-     * 権限
-     */
-    const SYSTEM_ADMIN = 0;
-    const SYSTEM_ADMIN_NAME = 'システム管理者';
-    const SITE_ADMIN = 1;
-    const SITE_ADMIN_NAME = 'サイト管理者';
-    const GENERAL_USER = 3;
-    const GENERAL_USER_NAME = '一般ユーザー';
 
     /**
      * 性別リスト
@@ -65,6 +78,16 @@ class CommonConst
     ];
 
     /**
+     * 権限
+     */
+    const SYSTEM_ADMIN = 0;
+    const SYSTEM_ADMIN_NAME = 'システム管理者';
+    const SITE_ADMIN = 1;
+    const SITE_ADMIN_NAME = 'サイト管理者';
+    const GENERAL_USER = 3;
+    const GENERAL_USER_NAME = '一般ユーザー';
+
+    /**
      * 権限リスト
      *
      * @param  array
@@ -72,27 +95,5 @@ class CommonConst
     const USER_REGISTER_MAIL_LIST = [
         self::SYSTEM_ADMIN => self::SYSTEM_ADMIN_NAME,
         self::SITE_ADMIN => self::SITE_ADMIN_NAME,
-    ];
-
-    /**
-     * top画像リスト
-     *
-     * @param  array
-     */
-    const TOP_IMAGE_LIST = [
-        1 =>  self::TOP_FILE_PATH_NAME_1,
-        2 =>  self::TOP_FILE_PATH_NAME_2,
-        3 =>  self::TOP_FILE_PATH_NAME_3,
-    ];
-
-    /**
-     * ドキュメントリスト
-     *
-     * @param  array
-     */
-    const DOCUMENT_LIST = [
-        1 =>  self::DOCUMENT_PATH_NAME_1,
-        2 =>  self::DOCUMENT_PATH_NAME_2,
-        3 =>  self::DOCUMENT_PATH_NAME_3,
     ];
 }
