1ccd7ece05331041344793a3a4072dddbde4d19b
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-04-04 13:14:39 +0900

    画像を真ん中に変更

--- resources/views/components/layout/top-hero.blade.php
+++ resources/views/components/layout/top-hero.blade.php
@@ -1,11 +1,11 @@
 <ul class="fixed top-0 left-0 -z-50 w-full">
     <li class="top-image h-[730px]">
-        <img src="{{ isset($topImagePath[1]) ? \Storage::url($topImagePath[1]) : 'images/top/top1.png'}}" alt="トップ画像" class="w-full h-full object-cover object-top">
+        <img src="{{ isset($topImagePath[1]) ? \Storage::url($topImagePath[1]) : 'images/top/top1.png'}}" alt="トップ画像" class="w-full h-full object-cover object-center">
     </li>
     <li class="top-image h-[730px]">
-        <img src="{{ isset($topImagePath[2]) ? \Storage::url($topImagePath[2]) : 'images/top/top2.png'}}" alt="トップ画像" class="w-full h-full object-cover object-top">
+        <img src="{{ isset($topImagePath[2]) ? \Storage::url($topImagePath[2]) : 'images/top/top2.png'}}" alt="トップ画像" class="w-full h-full object-cover object-center">
     </li>
     <li class="top-image h-[730px]">
-        <img src="{{ isset($topImagePath[3]) ? \Storage::url($topImagePath[3]) : 'images/top/top3.png'}}" alt="トップ画像" class="w-full h-full object-cover object-top">
+        <img src="{{ isset($topImagePath[3]) ? \Storage::url($topImagePath[3]) : 'images/top/top3.png'}}" alt="トップ画像" class="w-full h-full object-cover object-center">
     </li>
 </ul>
