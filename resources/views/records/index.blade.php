cf31e329a34a459d6d7219126d53ff5604cf1880
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-05-31 18:37:26 +0900

    大会記録画面追加

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -1,4 +1,15 @@
 <x-layout.layout>
     <!-- main -->
-    <section></section>
+    <section>
+        <x-layout.container>
+            <h2 class="text-4xl font-black my-14 sp:my-8">日本記録</h2>
+            <x-parts.button-arrow  bgColor='red'>
+                <a href="https://j-w-a.or.jp/ranking/man-2018-2021-ranking01/">日本記録はこちら</a>
+            </x-parts.button-arrow>
+            <h2 class="text-4xl font-black my-14 sp:my-8">世界記録</h2>
+            <x-parts.button-arrow bgColor='red'>
+                <a href="https://iwf.sport/results/world-records/">世界記録はこちら</a>
+            </x-parts.button-arrow>
+        </x-layout.container>
+    </section>
 </x-layout.layout>
