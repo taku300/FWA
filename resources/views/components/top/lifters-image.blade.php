370caaaa64b0f88b9eb7ad9a3556343952f1821f
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-06-20 22:23:49 +0900

    トップのリンクを解除

--- resources/views/components/top/lifters-image.blade.php
+++ resources/views/components/top/lifters-image.blade.php
@@ -2,6 +2,6 @@
     "source" => "images/top/lifter1.png",
     "alt" => "",
 ])
-<a href="{{ route('lifters.index') }}">
+<div>
     <img class="object-cover object-top aspect-[572/400] w-[570px] pc:w-[400px] sp:w-[270px]" src="{{ asset($source) }}" alt="選手">
-</a>
+</div>
