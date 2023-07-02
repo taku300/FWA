4ceefdd3b401a4aa7d666ccbcb21d7ec745be2c3
t-ookuma <siel0913@gmail.com>
2023-07-02 22:32:37 +0900

    request修正

--- app/Http/Requests/NewsForm.php
+++ app/Http/Requests/NewsForm.php
@@ -39,7 +39,7 @@ class NewsForm extends FormRequest
             'news_links.*.title' => ['required_with:news_links.*.link_path', 'string'],
             'news_links.*.link_path' => ['required_with:news_links.*.title', 'string'],
             'news_images' => 'array',
-            'news_images.*.news_image_path' => 'string',
+            'news_images.*.news_image_path' => 'image',
         ];
     }
 
